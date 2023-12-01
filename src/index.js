7a8996ecbf1bc3f7d61ea040ec9656f65f8c05ff
BorschVS <borschvadym@gmail.com>
2023-12-02 00:48:37 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,72 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './js/api';
+import { fetchCards } from './js/api';
+import cardTpl from './templates/card.hbs';
+import { resetPage } from './js/api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const lightboxOptions = {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  navText: ['‹', '›'],
+  fadeSpeed: 250,
+};
 
+refs.gallery.addEventListener('click', onGallery);
 
+function onGallery(e) {
+  e.preventDefault();
+  const lightbox = new SimpleLightbox(`.gallery a`, lightboxOptions);
+}
 
+Notify.init({
+  width: '300px',
 
+  position: 'center-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
+  timeout: 1000,
 
+  failure: {
+    background: 'rgb(160, 160, 160)',
+    textColor: '#000',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgb(0,0,0)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgb(0,0,0)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  },
+});
 
+refs.searchForm.addEventListener('submit', onSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
+function onSubmit(e) {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+  refs.loader.classList.remove('is-hidden');
+  resetPage();
+  fetchCards()
+    .then(articles => {
+      if (articles.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      markupCards({ articles });
+      refs.loader.classList.add('is-hidden');
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    })
+    .catch(error => console.log(error));
+}
 
+function onLoadMoreBtn() {
+  fetchCards().then(articles => {
+    markupCards({ articles });
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Notify.init({
-//   width: '250px',
-
-//   position: 'center-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-//   timeout: 2000,
-
-//   failure: {
-//     background: '#ffacac',
-//     textColor: '#fff',
-//     childClassName: 'notiflix-notify-failure',
-//     notiflixIconColor: 'rgba(0,0,0,0.2)',
-//     fontAwesomeClassName: 'fas fa-times-circle',
-//     fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-//     backOverlayColor: 'rgba(255,85,73,0.2)',
-//   },
-// });
+function markupCards(articles) {
+  refs.gallery.insertAdjacentHTML('beforeend', cardTpl(articles));
+}
--- src/js/api.js
+++ src/js/api.js
@@ -1,15 +1,35 @@
 import axios from 'axios';
 
+const API_KEY = '40756671-54754ada209148e7d7f60a97d';
 const BASE_URL = 'https://pixabay.com/api/';
+let page = 0;
 
 export const refs = {
-  searchForm: document.getElementById('#search-form'),
-  input: document.querySelector('imput[name="searchQuery"]'),
+  searchForm: document.getElementById('search-form'),
+  input: document.querySelector('input[name="searchQuery"]'),
   loader: document.querySelector('.loader'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-// axios.init({
-//   ''
-// })
+export function fetchCards() {
+  page += 1;
+  const perPage = 40;
+  const searchQuery = refs.input.value;
+  return axios
+    .get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(response => {
+      incrementPage();
+      return response.data.hits;
+    });
+}
 
-// axios.get('');
+function incrementPage() {
+  page += 1;
+}
+
+export function resetPage() {
+  page = 0;
+}
